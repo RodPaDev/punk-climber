3a59456f1557fa80f3d529c9cefbddb3d3aa48e6
Patrick Rodrigues <47316946+RodPaDev@users.noreply.github.com>
2022-03-13 11:43:44 +0000

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { ethers } from 'ethers'
+import './App.css'
+
+export default function App() {
+  const wave = () => {}
+
+  return (
+    <div className='mainContainer'>
+      <div className='dataContainer'>
+        <div className='header'>
+          <span role='img' aria-label='wave emoji'>
+            👋
+          </span>{' '}
+          Hey there!
+        </div>
+
+        <div className='bio'>
+          I am Patrick and I'm launchig myself into the web3 world. Want to
+          connect? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className='waveButton' onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  )
+}
